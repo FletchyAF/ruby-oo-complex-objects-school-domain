df36678008097fb63a836ed8f29b21897250f8c7
FletchyAF <joshua.f1998@gmail.com>
2021-01-22 11:24:57 -0500

    commit tree 4112709eb024118436c330ae3d9f584384080056

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(student, grade)
+        if !@roster.key?(grade)
+            @roster[grade] = []
+        end
+        @roster[grade].push(student)
+    end
+
+    def grade(grade_num)
+        return @roster[grade_num]
+    end
+
+    def sort
+        @roster.each_key {|key| @roster[key] = @roster[key].sort }
+        return @roster
+    end
+
+end
